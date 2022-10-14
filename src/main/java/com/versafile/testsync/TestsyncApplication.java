544827ef1ebdde29578e560271bcb0958c9d5dbb
rkosinray <38571854+rkosinray@users.noreply.github.com>
2022-10-14 16:16:43 -0700

    initial commit

---
+++ src/main/java/com/versafile/testsync/TestsyncApplication.java
@@ -0,0 +1,13 @@
+package com.versafile.testsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsyncApplication.class, args);
+	}
+
+}
